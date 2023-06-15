4f607cfeb2353a3bcff82243b0e12236dedbe641
ethanrutledge <rutledgethan@gmail.com>
2023-06-15 12:15:31 -0600

    added new distribution plots and refined trajectory sampling efficiency

--- DM-trajectory.py
+++ DM-trajectory.py
@@ -1,37 +1,47 @@
-# simulating DM trajectory through target box
+# simulating DM trajectory through DUNE detector
 
 import numpy as np
 import uproot_methods as urm
 import matplotlib.pyplot as plt
 
 # -------------------------DETECTOR GEOMETRY--------------------------------
-# unsure of actual dimensions of DUNE detector
-# temporarily using scale of 66m x 19m x 18m centered around the origin
-z_max = 19 / 2
-z_min = -19 / 2
-y_max = 9
-y_min = -9
-x_max = 33
-x_min = -33
+# based on dimensions of DUNE detector -- sourced from TDR doc
+# using scale of 12 x 14 x 58.2m centered around the origin
+x_scale = 12
+y_scale = 14
+z_scale = 58.2
+
+x_max = x_scale / 2
+x_min = -x_scale / 2
+y_max = y_scale / 2
+y_min = -y_scale / 2
+z_max = z_scale / 2
+z_min = -z_scale / 2
 
 # r_max
 # must be large enough s.t. no matter disk orientation target is covered
-r_max = np.sqrt((x_max - x_min) ** 2 + (y_max - y_min) ** 2 + (z_max - z_min) ** 2)
+r_max = np.sqrt((x_scale ** 2) + (y_scale ** 2) + (z_scale ** 2))/2
 
 # store found entry and exit points
 entries = []
 exits = []
+
+# store c and phis to check distribution
+cs = []
+phis = []
 
 # iterate for some number of samples of trajectories
 for j in range(100000):
     # ------------------------DM DISK ORIENTATION------------------------
     # sample c uniformly from -1 to 1
     c = 2.0 * np.random.random() - 1.0
+    cs.append(c)
     # convert c to theta(azimuthal angle)
     theta = np.arccos(c)
 
     # sample phi(polar angle) uniformly from 0 to 2pi
     phi = 2.0 * np.pi * np.random.random()
+    phis.append(phi)
 
     # sample alpha(angle about disk) uniformly from 0 to 2pi
     alpha = 2.0 * np.pi * np.random.random()
@@ -105,84 +115,6 @@ for j in range(100000):
 
 
 # -----------------------PLOT FUNCTIONS-----------------------------------
-def all_face_hex_dist_plot(points, point_type):
-    """
-    :param points: either entry or exit points list
-    :param point_type: string of either entry or exit
-    :return: creates figure of hex dist plot for all 6 faces of detector
-    """
-    x_lim = x_min, x_max
-    y_lim = y_min, y_max
-    z_lim = z_min, z_max
-
-    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
-
-    x0 = []
-    y0 = []
-    x1 = []
-    y1 = []
-    z2 = []
-    y2 = []
-    z3 = []
-    y3 = []
-    x4 = []
-    z4 = []
-    x5 = []
-    z5 = []
-
-    for i in range(len(points)):
-        if points[i].z == z_max:
-            x0.append(points[i].x)
-            y0.append(points[i].y)
-        elif points[i].z == z_min:
-            x1.append(points[i].x)
-            y1.append(points[i].y)
-        elif points[i].x == x_max:
-            z2.append(points[i].z)
-            y2.append(points[i].y)
-        elif points[i].x == x_min:
-            z3.append(points[i].z)
-            y3.append(points[i].y)
-        elif points[i].y == y_max:
-            x4.append(points[i].x)
-            z4.append(points[i].z)
-        elif points[i].y == y_min:
-            x5.append(points[i].x)
-            z5.append(points[i].z)
-
-    hb = axs[0, 0].hexbin(x0, y0, gridsize=10, cmap='inferno')
-    axs[0, 0].set(xlim=x_lim, ylim=y_lim)
-    axs[0, 0].set_title('z_max')
-    cb = fig.colorbar(hb, ax=axs[0, 0], label='counts')
-
-    hb = axs[1, 0].hexbin(x1, y1, gridsize=10, cmap='inferno')
-    axs[1, 0].set(xlim=x_lim, ylim=y_lim)
-    axs[1, 0].set_title('z_min')
-    cb = fig.colorbar(hb, ax=axs[1, 0], label='counts')
-
-    hb = axs[0, 1].hexbin(z2, y2, gridsize=10, cmap='inferno')
-    axs[0, 1].set(xlim=z_lim, ylim=y_lim)
-    axs[0, 1].set_title('x_max')
-    cb = fig.colorbar(hb, ax=axs[0, 1], label='counts')
-
-    hb = axs[1, 1].hexbin(z3, y3, gridsize=10, cmap='inferno')
-    axs[1, 1].set(xlim=z_lim, ylim=y_lim)
-    axs[1, 1].set_title('x_min')
-    cb = fig.colorbar(hb, ax=axs[1, 1], label='counts')
-
-    hb = axs[0, 2].hexbin(x4, z4, gridsize=10, cmap='inferno')
-    axs[0, 2].set(xlim=x_lim, ylim=z_lim)
-    axs[0, 2].set_title('y_max')
-    cb = fig.colorbar(hb, ax=axs[0, 2], label='counts')
-
-    hb = axs[1, 2].hexbin(x5, z5, gridsize=10, cmap='inferno')
-    axs[1, 2].set(xlim=x_lim, ylim=z_lim)
-    axs[1, 2].set_title('y_min')
-    cb = fig.colorbar(hb, ax=axs[1, 2], label='counts')
-
-    plt.savefig('allPlotsHexDist' + point_type)
-
-
 def single_face_hex_dist_plot(points, face, point_type):
     """
     :param points: either entries or exits list
@@ -286,9 +218,192 @@ def rotating_plot(entries, exits):
         plt.pause(.00001)
 
 
+def all_face_hex_dist_plot(points, point_type):
+    """
+    NEED TO NORMALIZE COLOR SCALE
+    :param points: either entry or exit points list
+    :param point_type: string of either entry or exit
+    :return: creates figure of hex dist plot for all 6 faces of detector
+    """
+    x_lim = x_min, x_max
+    y_lim = y_min, y_max
+    z_lim = z_min, z_max
+
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(20, 10))
+
+    x0 = []
+    y0 = []
+    x1 = []
+    y1 = []
+    z2 = []
+    y2 = []
+    z3 = []
+    y3 = []
+    x4 = []
+    z4 = []
+    x5 = []
+    z5 = []
+
+    for i in range(len(points)):
+        if points[i].z == z_max:
+            x0.append(points[i].x)
+            y0.append(points[i].y)
+        elif points[i].z == z_min:
+            x1.append(points[i].x)
+            y1.append(points[i].y)
+        elif points[i].x == x_max:
+            z2.append(points[i].z)
+            y2.append(points[i].y)
+        elif points[i].x == x_min:
+            z3.append(points[i].z)
+            y3.append(points[i].y)
+        elif points[i].y == y_max:
+            x4.append(points[i].x)
+            z4.append(points[i].z)
+        elif points[i].y == y_min:
+            x5.append(points[i].x)
+            z5.append(points[i].z)
+
+    hb = axs[0, 0].hexbin(x0, y0, gridsize=10, cmap='inferno')
+    axs[0, 0].set(xlim=x_lim, ylim=y_lim)
+    axs[0, 0].set_title('z_max')
+    cb = fig.colorbar(hb, ax=axs[0, 0], label='counts')
+
+    hb = axs[1, 0].hexbin(x1, y1, gridsize=10, cmap='inferno')
+    axs[1, 0].set(xlim=x_lim, ylim=y_lim)
+    axs[1, 0].set_title('z_min')
+    cb = fig.colorbar(hb, ax=axs[1, 0], label='counts')
+
+    hb = axs[0, 1].hexbin(z2, y2, gridsize=10, cmap='inferno')
+    axs[0, 1].set(xlim=z_lim, ylim=y_lim)
+    axs[0, 1].set_title('x_max')
+    cb = fig.colorbar(hb, ax=axs[0, 1], label='counts')
+
+    hb = axs[1, 1].hexbin(z3, y3, gridsize=10, cmap='inferno')
+    axs[1, 1].set(xlim=z_lim, ylim=y_lim)
+    axs[1, 1].set_title('x_min')
+    cb = fig.colorbar(hb, ax=axs[1, 1], label='counts')
+
+    hb = axs[0, 2].hexbin(x4, z4, gridsize=10, cmap='inferno')
+    axs[0, 2].set(xlim=x_lim, ylim=z_lim)
+    axs[0, 2].set_title('y_max')
+    cb = fig.colorbar(hb, ax=axs[0, 2], label='counts')
+
+    hb = axs[1, 2].hexbin(x5, z5, gridsize=10, cmap='inferno')
+    axs[1, 2].set(xlim=x_lim, ylim=z_lim)
+    axs[1, 2].set_title('y_min')
+    cb = fig.colorbar(hb, ax=axs[1, 2], label='counts')
+
+    plt.savefig('allPlotsHexDist' + point_type)
+
+
+def one_dim_hist_all_faces(points, point_type):
+    """
+    :param points: either entry or exit list
+    :param point_type: string of either "entry" or "exit"
+    :return: 12 histograms, 2 per face of point distribution & prints total points on each face
+    """
+    x_lim = x_min, x_max
+    y_lim = y_min, y_max
+    z_lim = z_min, z_max
+
+    fig, axs = plt.subplots(nrows=2, ncols=6, figsize=(40, 10))
+
+    x0 = []
+    y0 = []
+    x1 = []
+    y1 = []
+    z2 = []
+    y2 = []
+    z3 = []
+    y3 = []
+    x4 = []
+    z4 = []
+    x5 = []
+    z5 = []
+
+    for i in range(len(points)):
+        if points[i].z == z_max:
+            x0.append(points[i].x)
+            y0.append(points[i].y)
+        elif points[i].z == z_min:
+            x1.append(points[i].x)
+            y1.append(points[i].y)
+        elif points[i].x == x_max:
+            z2.append(points[i].z)
+            y2.append(points[i].y)
+        elif points[i].x == x_min:
+            z3.append(points[i].z)
+            y3.append(points[i].y)
+        elif points[i].y == y_max:
+            x4.append(points[i].x)
+            z4.append(points[i].z)
+        elif points[i].y == y_min:
+            x5.append(points[i].x)
+            z5.append(points[i].z)
+
+    axs[0, 0].hist(x0, range=x_lim)
+    axs[0, 0].set_title('face 0 @z_max x dim')
+    axs[1, 0].hist(y0, range=y_lim)
+    axs[1, 0].set_title('face 0 @z_max y dim')
+
+    axs[0, 1].hist(x1, range=x_lim)
+    axs[0, 1].set_title('face 1 @z_min x dim')
+    axs[1, 1].hist(y1, range=y_lim)
+    axs[1, 1].set_title('face 1 @z_min y dim')
+
+    axs[0, 2].hist(z2, range=z_lim)
+    axs[0, 2].set_title('face 2 @x_max z dim')
+    axs[1, 2].hist(y2, range=y_lim)
+    axs[1, 2].set_title('face 2 @x_max y dim')
+
+    axs[0, 3].hist(z3, range=z_lim)
+    axs[0, 3].set_title('face 3 @x_min z dim')
+    axs[1, 3].hist(y3, range=y_lim)
+    axs[1, 3].set_title('face 3 @x_min y dim')
+
+    axs[0, 4].hist(x4, range=x_lim)
+    axs[0, 4].set_title('face 4 @y_max x dim')
+    axs[1, 4].hist(z4, range=z_lim)
+    axs[1, 4].set_title('face 4 @y_max z dim')
+
+    axs[0, 5].hist(x5, range=x_lim)
+    axs[0, 5].set_title('face 5 @y_min x dim')
+    axs[1, 5].hist(z5, range=z_lim)
+    axs[1, 5].set_title('face 5 @y_min z dim')
+
+    plt.savefig('1DhistAllFaces' + point_type)
+
+    print(point_type)
+    print('face 0 total points: ', len(x0) + len(y0))
+    print('face 1 total points: ', len(x1) + len(y1))
+    print('face 2 total points: ', len(z2) + len(y2))
+    print('face 3 total points: ', len(z3) + len(y3))
+    print('face 4 total points: ', len(x4) + len(z4))
+    print('face 5 total points: ', len(x5) + len(z5))
+
+
+def costheta_phi_hist(cs, phis):
+    """
+    :param cs: set of all sampled c values c = cosTheta
+    :param phis: set of all sampled phi values
+    :return: histograms of both cosTheta and Phi
+    """
+
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+    axs[0].hist(cs)
+    axs[0].set_title('cosTheta')
+    axs[1].hist(phis)
+    axs[1].set_title('phi')
+    plt.savefig('cosThetaPhiDist')
+
+
 # ----------------------FUNCTION TEST CALLS------------------------
 # single_face_hex_dist_plot(exits, "y_min", "exit")
 # single_face_hex_dist_plot(entries, "z_max", "entry")
 # rotating_plot(entries, exits)
 # all_face_hex_dist_plot(entries, 'entry')
 # all_face_hex_dist_plot(exits, 'exit')
+# one_dim_hist_all_faces(entries, "entry")
+# one_dim_hist_all_faces(exits, "exit")
+costheta_phi_hist(cs, phis)
